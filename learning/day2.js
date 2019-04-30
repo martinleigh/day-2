834a7836111549ddc7388ebdd447cd4ab6057420
Ashley Bennett <codenation@CN-20.local>
2019-04-30 10:42:37 +0100

    day 2 notes

---
+++ learning/day2.js
@@ -0,0 +1,196 @@
+let num1 = 4;
+let num2 = 40;
+let num3 = 8;
+let num4 = 2;
+
+console.log(num1+num2)
+//adding num1 to num2
+console.log(num2-num3)
+//subtracting num2 with num3
+console.log(num1*num2-num4)
+//multiply num1 by num2 then subtracting by num3
+console.log(num3+num3/num1)
+
+console.log(num1*=num2)
+console.log(num1/=num2)
+
+
+numberOne = 10
+numberTwo = 30
+numberOne += numberTwo
+numberOne = numberOne + numberTwo
+console.log(numberOne)
+
+numberOne = 10
+numberTwo = 20
+numberOne -= numberTwo
+numberOne = numberOne - numberTwo
+console.log(numberOne)
+
+numberOne += numberTwo
+console.log(numberOne)
+//60
+numberOne -= numberTwo
+console.log(numberOne)
+
+// x += 1 same as : x = x + 1
+// x -= 1 same as : x = x - 1
+// x 
+
+var name = "martin"
+console.log(name)
+
+//let favouriteDrink = "coffee";
+//console.log (`My favourite drink is ${favouriteDrink}`)
+
+
+//let favouriteDrink = "tea";
+//console.log (`My favourite drink is ${favouriteDrink}`)
+
+var name = 'martin';
+var age ='37';
+var favouriteColour = 'blue';
+
+console.log (`My name is ${name} and My age is ${age} and My favouriteColour is ${favouriteColour}.`)
+//buts (name )(age)(favourite colour)in a sentence
+
+var doorOpen = "true"
+if  (doorOpen == "true")
+{
+    console.log("The door is open.")
+}
+else {
+    console.log("The door is closed.")
+}
+
+
+
+var age = 18
+if (age < 18)
+{
+console.log ("you cant buy that")
+}
+else if (age < 50)
+{
+console.log ("your old")
+}
+
+var age = 15
+if (age < 18)
+{
+console.log ("you cant buy that")
+}
+else if (age < 50)
+{
+console.log ("your old")
+}
+
+var age = 30
+if (age < 18)
+{
+console.log ("you cant buy that")
+}
+else if (age < 50)
+{
+console.log ("your old")
+}
+
+var str = "Hello my name is martin";
+console.log (str.slice(0, 5))
+console.log (str.search("name"))//
+console.log (str.replace("Hello", "hi"))//replaces old word to a new word
+console.log (str.lastIndexOf("n"))
+console.log (str.substring(0, 5))
+console.log (str.substr(0,7))
+console.log (str.charAt(7))
+console.log (str.concat(7))
+
+var realAge = 10
+if  (realAge >10)
+{
+    console.log("you are to young")
+}
+else if (realAge = 10)
+{
+    console.log("you are spot on")
+}
+else 
+{
+    console.log("you are old enough")
+}
+
+let dogFed = true
+
+function dogFeeding() {
+   if (dogFed) {
+      console.log('the dog is playing you')
+        console.log('i wont feed them') 
+        dogFed = false
+   }
+   else {
+       console.log('the dog needs feeding')
+       console.log('i have now fed the dog')
+       console.log('the dog has now eaten')
+       dogFed = true
+   }
+}
+var day = "saturday"
+
+function daychecker() {
+      if(day== "monday")
+      {
+          console.log(day)  
+          day = "tuesday"
+      }
+      else if (day== "tuesday")
+      {
+          console.log(day)
+          day = "wednesday"
+      }
+      else if (day== "wednesday")
+      {
+          console.log(day)
+          day = "thursday"
+      }
+      else if (day== "thursday")
+      {
+          console.log(day)
+          day = "friday"
+      }
+      else if (day== "friday")
+      {
+          console.log(day)
+          day = "saturday"
+      }
+      else if (day== "saturday")
+      {
+          console.log(day)
+          day = "sunday"
+      }
+      else if (day== "sunday")
+      {
+          console.log(day)
+          day = "monday"
+      }
+    }
+    
+daychecker()
+daychecker()
+daychecker()
+    
+      
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
